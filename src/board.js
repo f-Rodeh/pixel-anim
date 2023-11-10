d40f1e8151fabf50d2136ae125ff9aa3b8d44199
f-Rodeh <rodeh.dev@gmail.com>
2023-11-10 00:17:27 -0600

    Add cells to PixelBoard

--- src/board.js
+++ src/board.js
@@ -1,8 +1,23 @@
 export { PixelBoard };
 
+const Row = (itemCount) => {
+  const div = document.createElement("div");
+  div.classList.add("row");
+  for (let i = 0; i < itemCount; i++) {
+    const item = document.createElement("div");
+    div.appendChild(item);
+  }
+  return div;
+};
+
 const PixelBoard = (width, height) => {
   const element = document.createElement("div");
   element.classList.add("pixel-board");
+
+  for (let i = 0; i < height; i++) {
+    const row = Row(width);
+    element.appendChild(row);
+  }
 
   function getSize() {
     return { width, height };
